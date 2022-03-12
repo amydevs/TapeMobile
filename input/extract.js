2363c7fdefe69a9d58c68387b4ff5cf12cb3f1c2
Amy Y <ctrlalternativedelete@gmail.com>
2022-03-12 23:40:12 +1100

    added auto import for windows

--- input/extract.js
+++ input/extract.js
@@ -4,17 +4,20 @@ const fs = require('fs');
 const fse = require('fs-extra');
 
 const appoutputpath = path.join(__dirname, '../', 'app')
+
 const baseinputpath = path.join(__dirname)
+let asarpath = path.join(baseinputpath, 'app.asar')
+const _tmpasarpth = path.join(process.env.HOME, "AppData", String.raw`\Local\Programs\Tape\resources\app.asar`)
+if (fs.existsSync(_tmpasarpth)) {
+    asarpath = _tmpasarpth
+}
+
 const extractedpath = path.join(baseinputpath, 'app.extracted')
 
-asar.extractAll(path.join(baseinputpath, 'app.asar'), extractedpath)
+asar.extractAll(asarpath, extractedpath)
 fs.readdirSync(path.join(extractedpath, 'app')).forEach((e, i) => {
     fse.move(path.join(extractedpath, 'app', e), path.join(appoutputpath, e), { overwrite: true }, err => {
         if (err) return console.error(err)
     })
 })
 console.log('copied over all files')
-// fse.move('./input/app.extracted', './app', { overwrite: true }, err => {
-//     if (err) return console.error(err)
-//     console.log('copied over')
-// })
