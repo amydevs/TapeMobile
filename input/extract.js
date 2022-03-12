ebe98a9e0a79ac7d7bfb1d43b5ee5f24d2970212
amy <ctrlalternativedelete@gmail.com>
2022-03-12 23:21:00 +1100

    fixed up extractjs

--- input/extract.js
+++ input/extract.js
@@ -1,2 +1,20 @@
 const asar = require('asar');
-asar.extractAll('./input/app.asar', './input/app.extracted')
+const path = require('path')
+const fs = require('fs');
+const fse = require('fs-extra');
+
+const appoutputpath = path.join(__dirname, '../', 'app')
+const baseinputpath = path.join(__dirname)
+const extractedpath = path.join(baseinputpath, 'app.extracted')
+
+asar.extractAll(path.join(baseinputpath, 'app.asar'), extractedpath)
+fs.readdirSync(path.join(extractedpath, 'app')).forEach((e, i) => {
+    fse.move(path.join(extractedpath, 'app', e), path.join(appoutputpath, e), { overwrite: true }, err => {
+        if (err) return console.error(err)
+    })
+})
+console.log('copied over all files')
+// fse.move('./input/app.extracted', './app', { overwrite: true }, err => {
+//     if (err) return console.error(err)
+//     console.log('copied over')
+// })
