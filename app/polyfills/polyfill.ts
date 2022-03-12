52681b712d11e6576c715376c7d22dcf275f668d
amy <ctrlalternativedelete@gmail.com>
2022-03-12 16:40:14 +1100

    added ui stuff

--- app/polyfills/polyfill.ts
+++ app/polyfills/polyfill.ts
@@ -1,20 +1,47 @@
 import "../stylesheets/main.css";
 
+window.addEventListener('load', function () {
+    const styles = window.document.styleSheets;
+    for (const style of Object.values(styles)) {
+        for (const rule of Object.values(style.cssRules)) {
+            if (rule instanceof CSSStyleRule) {
+                if (rule.selectorText.includes(":hover")) {
+                    rule.selectorText = `${rule.selectorText}, ${rule.selectorText.replace(":hover", ".hover")}`;
+                    // rule.selectorText = rule.selectorText.replace(':hover', '')
+                    // console.log(rule.selectorText);
+                }
+            }
+        }
+    }
+    styles[0].insertRule(".item > #label { border-right: none !important }")
+    document.querySelector("#newCollection").classList.add("hover");
+
+    const nonhoversel = ".newItemControl:not(.hover)";
+    for (const hoverable of Object.values(document.querySelectorAll(nonhoversel))) {
+        hoverable.classList.add("hover");
+    }
+    const mutobs = new MutationObserver(e => {
+        for (const hoverable of Object.values(document.querySelectorAll(nonhoversel))) {
+            hoverable.classList.add("hover");
+        }
+    })
+    mutobs.observe(document.querySelector("#main"), {childList:true})
+})
+
+
+  
+
+declare var BrowserFS: any;
+
 BrowserFS.install(window);
   // Configures BrowserFS to use the LocalStorage file system.
 BrowserFS.configure({
     fs: "LocalStorage"
-}, function(e) {
+}, function(e: any) {
     if (e) {
         // An error happened!
         throw e;
     }
     // Otherwise, BrowserFS is ready-to-use!
 });
-
-const styles = window.document.styleSheets;
-for (const style of styles) {
-    style.cssRules
-}
-
 
