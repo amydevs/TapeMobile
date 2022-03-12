e11323539363afbddfb3dcba35d810d3602232bb
amy <ctrlalternativedelete@gmail.com>
2022-03-12 14:04:10 +1100

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const webpackConf = {
     context: path.resolve(__dirname, 'app'),
@@ -33,25 +34,36 @@ const webpackConf = {
         // NOTE: If you intend to use BrowserFS in a script tag, you do not need
         // to expose a BrowserFS global.
      
-        new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' })
+        new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' }),
+        new CopyPlugin({
+            patterns: [
+              { from: "stylesheets", to: "stylesheets" },
+            ],
+        }),
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (stats) => {
+                    
+                    const jsdom = require("jsdom");
+                    const { JSDOM } = jsdom;
+
+                    const fs = require('fs')
+                    const html = new JSDOM(fs.readFileSync('./app/app.html'), 'utf8');
+                    const document = html.window.document
+
+
+                    for (const [i, script] of document.querySelectorAll("head > script").entries()) {
+                        if (i == 0) {
+                            script.src = "app.js";
+                        }
+                        else { 
+                            script.remove();
+                        }
+                    }
+                    fs.writeFileSync('./dist/index.html', html.serialize())
+                });
+            }
+        }
     ]
 }
 module.exports = webpackConf;
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-const fs = require('fs')
-const html = new JSDOM(fs.readFileSync(path.join(webpackConf.context, 'app.html'), 'utf8'));
-const document = html.window.document
-
-
-for (const [i, script] of document.querySelectorAll("head > script").entries()) {
-    if (i == 0) {
-        script.src = "app.js";
-    }
-    else { 
-        script.remove();
-    }
-}
-fs.writeFileSync('./dist/index.html', html.serialize())
