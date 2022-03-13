654945f05f070a2940138bf7c31d3ebeaa9ba6e5
amy <ctrlalternativedelete@gmail.com>
2022-03-14 00:31:13 +1100

    added drag drop polyfill lib

--- app/polyfills/polyfill.ts
+++ app/polyfills/polyfill.ts
@@ -3,12 +3,13 @@ import enableBrowserFS from './modules/browser-fs'
 import enableStyles from './modules/styles'
 import enableSync from './modules/sync'
 import enableAppListeners from './modules/app'
+import "./modules/drag-drop-touch"
 
 (async () => {
     enableBrowserFS();
     enableAppListeners();
     enableStyles();
-    enableTouchDrag();
+    // enableTouchDrag();
     await enableSync();
 })()
 
