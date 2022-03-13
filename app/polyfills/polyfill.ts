c6d3c2a9b3e0fe0e4d833bbe9cf97ddd9fecaf52
Amy Y <ctrlalternativedelete@gmail.com>
2022-03-13 12:26:33 +1100

    added more ui stuff

--- app/polyfills/polyfill.ts
+++ app/polyfills/polyfill.ts
@@ -1,5 +1,6 @@
 import "../stylesheets/main.css";
 import {polyfill} from "mobile-drag-drop";
+import $ from "jquery";
 
 // optional import of scroll behaviour
 import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
@@ -16,6 +17,8 @@ document.addEventListener("dragenter", (event)=> {
 window.addEventListener( 'touchmove', function() {}, {passive: false});
 
 window.addEventListener('load', function () {
+
+    // polyfill all hovers to also work with a ".hover" utility class
     const styles = window.document.styleSheets;
     for (const style of Object.values(styles)) {
         for (const rule of Object.values(style.cssRules)) {
@@ -28,29 +31,64 @@ window.addEventListener('load', function () {
             }
         }
     }
-    styles[0].insertRule(".item > #label { border-right: none !important }")
-    document.querySelector("#newCollection").classList.add("hover");
-
+    
+    // polyfill hovers to always trigger
     const nonhoversel = ".newItemControl:not(.hover)";
-    for (const hoverable of Object.values(document.querySelectorAll(nonhoversel))) {
-        hoverable.classList.add("hover");
-    }
+    $(nonhoversel).toggleClass("hover");
+    document.querySelector("#newCollection").classList.add("hover");
     const mutobs = new MutationObserver(e => {
-        for (const hoverable of Object.values(document.querySelectorAll(nonhoversel))) {
-            hoverable.classList.add("hover");
-        }
+        $(nonhoversel).toggleClass("hover");
+        // for (const hoverable of Object.values(document.querySelectorAll(nonhoversel))) {
+        //     hoverable.classList.add("hover");
+        // }
     })
     mutobs.observe(document.querySelector("#main"), {childList:true})
+    
+    // fix for drag over
     const errFunc = window.onerror.bind({});
     window.onerror = function myErrorHandler(error, url, lineNumber) {
         if (error.toString().includes("'length'")) {}
         else {errFunc(error, url, lineNumber)}
-        
     };
 
+    // remove border on label for items
+    styles[0].insertRule(".item > #label { border-right: none !important }");
+
+    // make graph bigger for touch devices
     styles[0].insertRule(".group > .graph { min-height: 12px !important; position: relative; }")
     styles[0].insertRule('.group > .graph::after { content:""; display: block; position: absolute; left: 40%; right: 40%; bottom: 5px; height: 1px; background: rgb(246, 245, 240); transition-property: opacity, visibility; transition-duration: 0.4s, 0.3s; transition-timing-function: ease, ease; transition-delay: 0.2s; }')
     styles[0].insertRule('.group > .graph:hover::after { opacity: 0 }')
+    
+    
+    const toolbar = document.createElement("div")
+    toolbar.style.marginTop = "10px"
+    toolbar.style.justifyContent = "space-around"
+    toolbar.style.textAlign = "center"
+    toolbar.classList.add("status")
+
+    const searchbuttton = document.createElement("div")
+    searchbuttton.innerText = "Search"
+    searchbuttton.onclick = (e) => {
+        document.dispatchEvent(new KeyboardEvent('keydown', {
+            key: 'f',
+            keyCode: 70,
+            ctrlKey: true,
+        }));
+    }
+    toolbar.appendChild(searchbuttton)
+
+    const hidebutton = document.createElement("div")
+    hidebutton.innerText = "Hide All"
+    hidebutton.onclick = (e) => {
+        document.dispatchEvent(new KeyboardEvent('keydown', {
+            key: 'f',
+            keyCode: 70,
+            ctrlKey: true,
+        }));
+    }
+    toolbar.appendChild(hidebutton)
+    
+    document.querySelector("header > .group").appendChild(toolbar)
 })  
 
 declare var BrowserFS: any;
