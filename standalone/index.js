d66b7ccabf1ad24b0906c22b40b59f3c4da5ffb8
amy <ctrlalternativedelete@gmail.com>
2022-03-15 22:06:23 +1100

    fixed standalone

--- standalone/index.js
+++ standalone/index.js
@@ -1,13 +1,16 @@
-require("../input/extract.js")
 const webpack = require('webpack');
 const webpackcfg = require('../webpack.config');
 const WebpackDevServer = require('webpack-dev-server');
+const path = require("path");
 
 (async () => {
     process.env.NODE_ENV = "production";
     process.env.NODE_PWA = "true";
 
-    const server = new WebpackDevServer(webpack(webpackcfg));
+    require("../input/extract.js");
+
+    process.cwd(path.join(__dirname, "../"));
+    const server = new WebpackDevServer(webpackcfg.devServer, webpack(webpackcfg));
 
     await server.start()
 })()
